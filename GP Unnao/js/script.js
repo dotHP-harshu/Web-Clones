2bff846415f1afbb303c2059b1a061138df0c37d
dotHP-harshu <harshprajapaticoding@gmail.com>
2024-05-23 20:51:33 +0530

    added Animations and Pages

--- GP Unnao/js/script.js
+++ GP Unnao/js/script.js
@@ -1,5 +1,16 @@
 let bars = document.querySelector("#bars");
 
+let facultyCardOdd = document.querySelectorAll(".faculty-card:nth-child(odd)");
+let facultyCardEven = document.querySelectorAll(
+  ".faculty-card:nth-child(even)"
+);
+facultyCardOdd.forEach((card) => {
+  card.setAttribute("data-aos", "fade-right");
+});
+facultyCardEven.forEach((card) => {
+  card.setAttribute("data-aos", "fade-left");
+});
+
 bars.addEventListener("click", () => {
   document.querySelector("nav").classList.toggle("display-block");
 });
